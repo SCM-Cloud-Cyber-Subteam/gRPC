3f2c649fe6a6f434ef89e945f8795a92caa7fe68
root <root@ME-VRMILLING6.adsroot.itcs.umich.edu>
2019-04-24 12:50:31 -0400

    Test cases added for time analysis

--- src/main/java/com/scm/grpc/ScmGrpcServer.java
+++ src/main/java/com/scm/grpc/ScmGrpcServer.java
@@ -22,7 +22,7 @@ import org.json.simple.JSONArray;
 public class ScmGrpcServer {
 	public static void main(String [] args) throws IOException, InterruptedException {
 		System.out.println("SecCldMfg Grpc Server Started");
-		Server server = ServerBuilder.forPort(50501)
+		Server server = ServerBuilder.forPort(50051)
 			.addService(new MessageHandlerImpl())
 			.build();
 		server.start();
@@ -51,8 +51,26 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 	@Override
 	public void getGroups(GroupsRequest request, StreamObserver<GroupsReply> replyObserver){
 		
-		List<String> groupsList = OPCDataExtractor.getExistingGroups();
+		List<String> groupsList = /*new ArrayList<String>();*/OPCDataExtractor.getExistingGroups();
 		
+		/*
+		//Use this for testing
+		groupsList.add("AB");
+		groupsList.add("AdvManLab");
+		groupsList.add("CNC1");
+		groupsList.add("CNC2");
+		groupsList.add("CNC3");
+		groupsList.add("CNC4");
+		groupsList.add("Counting_number_of_IO_points");
+		groupsList.add("Emu_PLC0");
+		groupsList.add("Emu_PLC1");
+		groupsList.add("JADE");
+		groupsList.add("Jenny_branch");
+		groupsList.add("PLC0");
+		groupsList.add("ProjectTemplate");
+		groupsList.add("TestProject");
+		*/
+
 		GroupsReply reply = GroupsReply.newBuilder()
 			.addAllGroupNames(groupsList)
 			.build();
@@ -60,13 +78,31 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 		replyObserver.onNext(reply);
 		replyObserver.onCompleted();
 	}
-
+	
 	@Override
 	public void getGroupTags(GroupTagsRequest request, StreamObserver<GroupTagsReply> replyObserver){
 		
 		String groupName = request.getGroupName();
 
-		List<String> groupTagsList = OPCDataExtractor.getAvailableTagsInGroup(groupName);
+		List<String> groupTagsList = /*new ArrayList<String>();*/OPCDataExtractor.getExistingGroups();
+		
+		/*
+		//Use this for testing (This is data for CNC1 for reference)
+		groupTagsList.add("ACTIVE_SCREEN");
+		groupTagsList.add("ANGLE");
+		groupTagsList.add("ANGLE1");
+		groupTagsList.add("ANGLE2");
+		groupTagsList.add("ANGLE_C");
+		groupTagsList.add("ANGLE_C_IJ");
+		groupTagsList.add("ANGLE_NEW");
+		groupTagsList.add("ANGLE_NEW1");
+		groupTagsList.add("ANGLE_OFFSET");
+		groupTagsList.add("ANGLE_RAD");
+		groupTagsList.add("AVG_CYCLE");
+		groupTagsList.add("AVG_CYCLE_TIME");
+		groupTagsList.add("AVG_CYCLES");
+		groupTagsList.add("AX0_E_RETURN_POSITION");
+		*/
 
 		GroupTagsReply reply = GroupTagsReply.newBuilder()
 			.addAllTagNames(groupTagsList)
@@ -75,7 +111,7 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 		replyObserver.onNext(reply);
 		replyObserver.onCompleted();
 	}
-
+	
 	@Override
 	public void getTag(TagRequest request, StreamObserver<TagReply> replyObserver){
 
@@ -83,13 +119,34 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 		String groupName = request.getGroupName();
 		JSONObject tagData = OPCDataExtractor.getTag(tagName, groupName);
 	
+		/*
+		// Use this for testing. These values are random and not part of anything
+		JSONObject tagData = new JSONObject();
+		//add client handle
+		tagData.put("clientHandle", 0);
+		//add item name
+		tagData.put("itemName", "MISC_ITEM");
+		//add item  activity
+		tagData.put("active", true);
+		//add access path
+		tagData.put("accessPath", "MachineName");
+		//add time stamp
+		tagData.put("timeStamp", ":Wed Apr 24 11:42:36 EDT 2019");
+		//add item data type
+		tagData.put("dataType", "VT_R4");
+		//add item value
+		tagData.put("value", 0);
+		//add item quality
+		tagData.put("quality", true);
+		*/
 
+		
 		TagReply reply = TagReply.newBuilder()
 			.setClientHandle(tagData.get("clientHandle").toString())
 			.setItemName(tagData.get("itemName").toString())
 			.setActive(Boolean.valueOf(tagData.get("active").toString()))
 			.setAccessPath(tagData.get("accessPath").toString())
-			.setTimeStamp(Long.valueOf(tagData.get("timeStamp").toString()))
+			.setTimeStamp(4242019)
 			.setDataType(tagData.get("dataType").toString())
 			.setValue(tagData.get("value").toString())
 			.setQuality(Boolean.valueOf(tagData.get("quality").toString()))
@@ -99,6 +156,7 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 		replyObserver.onCompleted();
 	}
 
+	/*
 	@Override
 	public void getMultipleTags(MultipleTagsRequest request, StreamObserver<MultipleTagsReply> replyObserver) {
 		
@@ -137,7 +195,7 @@ class MessageHandlerImpl extends MessageHandlerGrpc.MessageHandlerImplBase {
 		replyObserver.onNext(reply);
 		replyObserver.onCompleted();
 	}
-	
+	*/
 	
  
 }
